22fec873ea97987e0414438d4bbe2d8672659f4e
Charles Boyce <wxaz020@live.rhul.ac.uk>
2017-12-16 15:26:20 +0100

    FEAT: new endpoint: get all

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ const getRandomPallette = () => {
   return palettes[getRandomPalletteIndex()];
 }
 
+const getAllPallettes = () => {
+  return palettes;
+}
+
 app.use(cors());
 
-app.get('/', (req, res) => res.send(getRandomPallette()))
+app.get('/', (req, res) => res.send(getRandomPallette()));
+app.get('/all', (req, res) => res.send(getAllPallettes()));
 
 app.listen(process.env.PORT || 3000);
